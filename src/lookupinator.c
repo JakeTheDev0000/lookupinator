24a358a21787bf7cfa7a659381dde68892c40686
Jacob O'Brien <jacobrien64@gmail.com>
2022-10-16 14:44:10 -0700

    added lists and an exit, added \'displayUsrInfo\' function

--- src/lookupinator.c
+++ src/lookupinator.c
@@ -1,34 +1,61 @@
 #include <stdio.h>
 #include <string.h>
 
+int displayUsrInfo(int nameID);
+
 int main() {
 	char whatNameLookup[30];
 	const char *names[] = {
 		"jake",
 		"richie",
-		"emilio"
+		"emilio",
+		"chi",
+		"immaculata",
+		"lissy",
+		"fransico",
+		"joey",
+		"jason"
 	};
 	int NAME_CAPT = sizeof names / sizeof names[0];
+	int needCheckName = 0;
 
 	while(1) {
-		printf("Enter a name >> ");
+		printf("\n\nEnter a name >> ");
 		scanf("%s", &whatNameLookup);
+		needCheckName = 0;
 
-		int I_NAME_CHECKER = 0;
-		for (I_NAME_CHECKER = 0; I_NAME_CHECKER < NAME_CAPT; I_NAME_CHECKER++) {
-			if (strcmp(whatNameLookup, names[I_NAME_CHECKER]) == 0) {
-				printf("found %s\n", names[I_NAME_CHECKER]);
-			}
-			else {
-				printf("%s is not %s\n", whatNameLookup, names[I_NAME_CHECKER]);
-			}
+		if (strcmp(whatNameLookup, "exit") == 0) {
+			return 0;
 		}
-	}
+		else if (strcmp(whatNameLookup, "list") == 0) {
+			int I_LIST = 0;
+			for(I_LIST = 0; I_LIST < NAME_CAPT; I_LIST++) {
+				printf("%d - %s\n",I_LIST, names[I_LIST]);
+			}
+			needCheckName = 1;
+			
+		}
+		
+		// I know its a mess down here :/ 
+		if (needCheckName == 0){
+			int I_NAME_CHECKER = 0;
+			for (I_NAME_CHECKER = 0; I_NAME_CHECKER < NAME_CAPT; I_NAME_CHECKER++) {
+				if (strcmp(whatNameLookup, names[I_NAME_CHECKER]) == 0) {
+					printf("found %s\n", names[I_NAME_CHECKER]);
+					displayUsrInfo(I_NAME_CHECKER);
+					break;
+				}
+				else {
+					printf("%s is not %s\n", whatNameLookup, names[I_NAME_CHECKER]);
+				} // else above
+			} // for loop
+		} // if needCheckName
+	} // while loop
 
 
     return 0;
 }
 
-void displayUsrInfo() {
-
+int displayUsrInfo(int nameID) {
+	printf("%d\n", nameID);
 }
